bb2f804254c3d0eaeb3189a937ace47e8dba29a4
Simon-Fuhaoyuan <Simon-Fuhaoyuan@sjtu.edu.cn>
2020-06-02 21:57:59 +0800

    finish initial functions

--- main.py
+++ main.py
@@ -11,11 +11,11 @@ import torch.utils.data as Data
 import torch.optim as optim
 import torchvision.transforms as transforms
 
-from vocData import vocData
+from vocData import vocData, 
 from utils.transform import MaskToTensor
 # from evaluate import accuracy
 # import models
-from utils.functions import get_criterion
+from utils.functions import get_criterion, train, test
 
 
 ch = logging.StreamHandler(sys.stdout)
@@ -31,6 +31,7 @@ def parser_args():
     parser.add_argument('--batch_size', help='Batch size', default=1, type=int)
     parser.add_argument('--loss', help='The loss function', default='crossentropy', type=str)
     parser.add_argument('--lr', help='The learning rate', default=0.001, type=float)
+    parser.add_argument('--ignore_label', help='The index of label for ignore', default=255, type=int)
     parser.add_argument('--root', help='The initial dataset root', default='./VOCdevkit/VOC2012', type=str)
     parser.add_argument('--weight', help='The weight folder', default='./weights', type=str)
     parser.add_argument('--model', help='The architecture of CNN', default='FCN8s', type=str)
@@ -51,12 +52,33 @@ def main(net, dataloader, device, config):
         weight_decay=config.weight_decay)
     scheduler = optim.lr_scheduler.ExponentialLR(optimizer, 0.95)
 
-    crit = get_criterion(config)
+    crit = get_criterion(config, ignore_label=config.ignore_label)
     crit = crit.to(device)
 
     if not os.path.isdir(config.weight):
         os.makedirs(config.weight)
     checkpoint = os.path.join(config.weight, config.model + '.pth')
+
+    for epoch in range(config.epoch):
+        ########### TRAIN ##########
+        start = time()
+        loss_train = train(config, net, device, train_loader, crit, optimizer, epoch)
+        end = time() - end
+        logging.info(
+            f'=> Epoch[{epoch}] finished, Average train Loss: {loss_train:.3f}, Tot Time: {end:.3f}'
+        )
+        ########### TEST ###########
+        start = time()
+        PA, mPA, mIoU = test(config, net, device, test_loader, epoch)
+        end = time() - end
+        logging.info(
+            f'=> Epoch[{epoch}] Test Result'
+        )
+        logging.info(
+            f'=> Pixel Accuracy: {PA:.3f} | Mean Pixel Accuracy: {mPA:.3f} | Mean IoU: {mIoU:.3f}'
+        )
+        torch.save(net.state_dict(), checkpoint)
+
 
 if __name__ == '__main__':
     config = parser_args()
@@ -67,6 +89,7 @@ if __name__ == '__main__':
     net.to(device)
 
     # transform of images
+    mean_std = ([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
     input_transform = transforms.Compose([
         transforms.ToTensor(),
         transforms.Normalize(*mean_std)
@@ -87,7 +110,7 @@ if __name__ == '__main__':
 
     test_dataset = vocData(
         config.root, 
-        'test', 
+        'val', 
         transform=input_transform, 
         mask_transform=mask_transform)
     test_loader = Data.DataLoader(
