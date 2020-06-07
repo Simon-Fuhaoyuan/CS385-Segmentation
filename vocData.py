e356358869cdd9cc86fa9f2adc3c44d95518662d
Simon-Fuhaoyuan <simon-fuhaoyuan@sjtu.edu.cn>
2020-06-07 16:15:53 +0800

    add train sbd dataset

--- vocData.py
+++ vocData.py
@@ -37,13 +37,23 @@ class vocData(Dataset):
     
     def make_dataset(self):
         items = []
-        img_root = os.path.join(self.root, 'JPEGImages')
-        mask_root = os.path.join(self.root, 'SegmentationClass')
-        data_list = [l.strip('\n') for l in open(os.path.join(
-            self.root, 'ImageSets', 'Segmentation', self.phase + '.txt')).readlines()]
-        for it in data_list:
-            item = (os.path.join(img_root, it + '.jpg'), os.path.join(mask_root, it + '.png'))
-            items.append(item)
+
+        if self.root[-4:] == '2012': # VOC2012
+            img_root = os.path.join(self.root, 'JPEGImages')
+            mask_root = os.path.join(self.root, 'SegmentationClass')
+            data_list = [l.strip('\n') for l in open(os.path.join(
+                self.root, 'ImageSets', 'Segmentation', self.phase + '.txt')).readlines()]
+            for it in data_list:
+                item = (os.path.join(img_root, it + '.jpg'), os.path.join(mask_root, it + '.png'))
+                items.append(item)
+        else: # VOC SBD
+            img_root = os.path.join(self.root, 'img')
+            mask_root = os.path.join(self.root, 'SegmentationClass')
+            data_list = [l.strip('\n') for l in open(os.path.join(
+                self.root, self.phase + '.txt')).readlines()]
+            for it in data_list:
+                item = (os.path.join(img_root, it + '.jpg'), os.path.join(mask_root, it + '.png'))
+                items.append(item)
 
         return items
 
