50eb7b2d2972d603ba2da131cf022d9f1a009a94
Simon-Fuhaoyuan <simon-fuhaoyuan@sjtu.edu.cn>
2020-06-09 15:59:03 +0800

    add unet

--- models/__init__.py
+++ models/__init__.py
@@ -1,1 +1,2 @@
 import models.FCN8s
+import models.unet
--- models/utils.py
+++ models/utils.py
@@ -1,6 +1,7 @@
 import numpy as np
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 
 
 def get_upsampling_weight(in_channels, out_channels, kernel_size):
@@ -17,3 +18,44 @@ def get_upsampling_weight(in_channels, out_channels, kernel_size):
                       dtype=np.float64)
     weight[range(in_channels), range(out_channels), :, :] = filt
     return torch.from_numpy(weight).float()
+
+
+class unetConv2(nn.Module):
+    def __init__(self, in_size, out_size, is_batchnorm):
+        super(unetConv2, self).__init__()
+
+        if is_batchnorm:
+            self.conv1 = nn.Sequential(
+                nn.Conv2d(in_size, out_size, 3, 1, 1), nn.BatchNorm2d(out_size), nn.ReLU()
+            )
+            self.conv2 = nn.Sequential(
+                nn.Conv2d(out_size, out_size, 3, 1, 1), nn.BatchNorm2d(out_size), nn.ReLU()
+            )
+        else:
+            self.conv1 = nn.Sequential(nn.Conv2d(in_size, out_size, 3, 1, 1), nn.ReLU())
+            self.conv2 = nn.Sequential(nn.Conv2d(out_size, out_size, 3, 1, 1), nn.ReLU())
+
+    def forward(self, inputs):
+        outputs = self.conv1(inputs)
+        outputs = self.conv2(outputs)
+        return outputs
+
+
+class unetUp(nn.Module):
+    def __init__(self, in_size, out_size, is_deconv):
+        super(unetUp, self).__init__()
+        self.conv = unetConv2(in_size, out_size, False)
+        if is_deconv:
+            self.up = nn.ConvTranspose2d(in_size, out_size, kernel_size=2, stride=2)
+        else:
+            self.up = nn.UpsamplingBilinear2d(scale_factor=2)
+
+    def forward(self, inputs1, inputs2):
+        outputs2 = self.up(inputs2)
+
+        offset_w = inputs1.size()[3] - outputs2.size()[3]
+        offset_h = inputs1.size()[2] - outputs2.size()[2]
+        
+        padding = [offset_w // 2, offset_w - offset_w // 2, offset_h // 2, offset_h - offset_h // 2]
+        outputs2 = F.pad(outputs2, padding)
+        return self.conv(torch.cat([inputs1, outputs2], 1))
